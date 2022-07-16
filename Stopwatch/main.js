91e1acdb992a266f2b9e52f17488e064b290969b
Joe-Lindie <josephlindie@hotmail.co.uk>
2022-07-16 18:39:33 +0100

    basic HTML markup, style, and logic

---
+++ Stopwatch/main.js
@@ -0,0 +1,23 @@
+//GRAB ELEMENTS FROM DOM
+const startBtn = document.getElementById("start_button")
+const pauseBtn = document.getElementById("pause_button")
+const resettBtn = document.getElementById("reset_button")
+const timerOutput = document.getElementById("output")
+
+//EVENTLISTENERS
+startBtn.addEventListener("click", startTimerFn)
+pauseBtn.addEventListener("click", pauseTimerFn)
+resettBtn.addEventListener("click", resetTimerFn)
+
+//FUNCTIONS
+function startTimerFn() {
+  // add start timer logic here
+}
+
+function pauseTimerFn() {
+  // add pause timer logic here
+}
+
+function resetTimerFn() {
+  // add reset timer logic here
+}
